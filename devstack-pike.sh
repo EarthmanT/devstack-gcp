2a4accc7d0448e87cd58acba38bb2d4e9fbf3f82
EarthmanT <EarthmanT@users.noreply.github.com>
2020-12-22 10:11:13 +0200

    Update devstack-pike.sh

--- devstack-pike.sh
+++ devstack-pike.sh
@@ -3,12 +3,18 @@
 DEBIAN_FRONTEND=noninteractive sudo apt-get -y update
 DEBIAN_FRONTEND=noninteractive sudo apt-get upgrade -y
 
+sudo apt-get -y install git vim-gtk libxml2-dev libxslt1-dev libpq-dev python-pip libsqlite3-dev
+sudo apt-get -y build-dep python-mysqldb && sudo pip install git-review tox
+
 # Get External IP of this Instance
 externalip=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
 
 # Clone devstack Pike repo
 
 git clone https://git.openstack.org/openstack-dev/devstack -b stable/pike
+git clone https://github.com/openstack/manila.git -b pike-em
+cp ./manila/contrib/devstack/lib/manila ./devstack/lib
+cp ./manila/contrib/devstack/extras.d/70-manila.sh ./devstack/extras.d/
 
 cd devstack
 
@@ -23,6 +29,8 @@ SERVICE_PASSWORD=openstack
 # Configure Nova novnc Proxy Base URL with External IP of this Instance
 NOVNCPROXY_URL=http://$externalip:6080/vnc_auto.html
 enable_plugin manila https://github.com/openstack/manila
+#manila
+ENABLED_SERVICES+=,manila,m-api,m-sch,m-shr
 
 # # Enable Heat
 # enable_plugin heat https://git.openstack.org/openstack/heat stable/pike
